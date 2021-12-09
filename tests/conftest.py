980948a10c1c1714bef34eac43e852da2f59a42e
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-12-09 18:09:19 +0200

    Remove unused fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,3 @@ from pytest import fixture
 @fixture
 def fixtures_path() -> Path:
     return Path(__file__).parent / "fixtures"
-
-
-@fixture
-def tmp_file(tmp_path: Path) -> Path:
-    return tmp_path / "tmp"
